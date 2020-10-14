20682be7529c0ea77ea47542beb6b87c54266938
shekharAggarwal <aggarwalshekhar99@gmail.com>
2020-10-14 11:02:09 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/videocall_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.videocall_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = 'eb82eb1c188d4b61b169ca1b90ee88a4';
